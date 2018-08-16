9dfd552385cd0cb56855d19679ce6f97007a98b2
tenderribs <dc3dude@gmail.com>
2018-08-16 10:39:06 -0900

    fadeLed.py: make a LED fade in and out with PWM

--- controlLights.py
+++ controlLights.py
@@ -1,19 +1,19 @@
-import time
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
-p.start(0)
-try:
-    while 1:
-        for dc in range(0, 101, 5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
-        for dc in range(100, -1, -5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
-except KeyboardInterrupt:
-    pass
-p.stop()
-GPIO.cleanup()
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False)
+
+GPIO.setup(2,GPIO.OUT)
+
+print("LED on")
+
+GPIO.output(2,GPIO.HIGH)
+
+time.sleep(1)
+
+print("LED off")
+
+GPIO.output(2,GPIO.LOW)
